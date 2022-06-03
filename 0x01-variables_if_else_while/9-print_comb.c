86b374a453321a3a38982b894dccc22d8969f62f
Sofiyyah1 <sofiyattaiwo139@gmail.com>
2022-06-03 13:50:32 -0700

    Task 9 added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,12 +4,10 @@
  * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Sucess)
  */
-
 int main(void)
-
 {
 	int n;
-	
+
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
